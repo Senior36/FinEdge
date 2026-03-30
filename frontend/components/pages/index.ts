72702277d2113c43215f3061a9b03360c11cca78
Arya-Sankhe <arsanke3@gmail.com>
2026-03-31 01:21:03 +0400

    Add fundamental analysis page and separate analysis views

---
+++ frontend/components/pages/index.ts
@@ -0,0 +1,3 @@
+export { default as TechnicalAnalysisPage } from './technical/TechnicalAnalysisPage';
+export { default as FundamentalAnalysisPage } from './fundamental/FundamentalAnalysisPage';
+export { default as SentimentAnalysisPage } from './sentiment/SentimentAnalysisPage';
